59530bb4529fe12f14f0a56ab69702a069654ff1
DBeidachazi <269502169@qq.com>
2023-07-07 14:32:47 +0800

    额度页面内容添加

--- src/views/quota/myquota.vue
+++ src/views/quota/myquota.vue
@@ -1,7 +1,77 @@
 <template>
-
-  22222
+  <div f-c-c>
+    <n-card title="我的额度" bordered mx-10 my-10 rounded-10>
+      <n-data-table :columns="columns" :data="data" :pagination="pagination" :bordered="false" align="center" />
+    </n-card>
+  </div>
 </template>
 
 <script setup>
+import { h } from "vue";
+import { NButton, useMessage, NProgress } from "naive-ui";
+
+
+
+const createColumns = ({
+  play
+}) => {
+  return [
+    {
+      title: "请求类别",
+      key: "type"
+    },
+    {
+      title: "本月调用量",
+      key: "title",
+      render(row) {
+        return h(
+          NProgress,
+          {
+            type: "line",
+            percentage: row.callVolumes,
+            showText: true,
+          }
+        )
+      }
+    },
+    {
+      title: "今日调用次数",
+      key: "callNumbers"
+    },
+    {
+      title: "每日调用额度(次/天)",
+      key: "DailyCallLimit",
+    },
+    {
+      title: "actions",
+      key: "actions",
+      render(row) {
+        return h(
+          NButton,
+          {
+            strong: true,
+            tertiary: true,
+            size: "small",
+            onClick: () => play(row)
+          },
+          { default: () => "Play" }
+        );
+      }
+    }
+  ];
+};
+
+const data = [
+  { type: "什么种类", callNumbers: "Wonderwall", DailyCallLimit: 23, callVolumes: 23 },
+  { type: 4, callNumbers: "Don't Look Back in Anger", DailyCallLimit: 123, callVolumes: 44 },
+  { type: 12, callNumbers: "Champagne Supernova", DailyCallLimit: 333, callVolumes: 55 }
+];
+
+const message = useMessage();
+let columns = createColumns({
+  play(row) {
+    message.info(`Play ${row.title}`);
+  }
+})
+let pagination = false
 </script>
--- src/views/quota/quotanalysis.vue
+++ src/views/quota/quotanalysis.vue
@@ -1,6 +1,21 @@
 <template>
+  <div f-c-c>
+    <n-card title="额度分析" bordered mx-10 my-10 rounded-10>
+      <div f-c-c>
+        <WorkbenchCard title="title" value="123321" />
+        <WorkbenchCard title="title" />
+        <WorkbenchCard />
+        <WorkbenchCard />
+      </div>
 
+      <div my-20>
+        <div class="chart-container" ref="chartContainer"></div>
+      </div>
+    </n-card>
+  </div>
 </template>
 
 <script setup>
+import WorkbenchCard from '~/src/components/card/WorkbenchCard.vue';
+
 </script>
